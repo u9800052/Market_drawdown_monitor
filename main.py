ac10f50ae0bd92d0fab4af01aad637ad1989db87
example-git-studentget <u9800052@gmail.com>
2026-02-02 21:36:07 +0800

    change decimal

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ try:
                 data_changed = True
 
             if drawdown <= stock['Threshold']:
-                send_line_message(f"前一交易日「{ticker}」自高點回撤「{drawdown*100:.0f}%」，下跌超過{stock['Threshold']*100:.2f}%，考慮買進！")
+                send_line_message(f"前一交易日「{ticker}」自高點回撤「{drawdown*100:.2f}%」，下跌超過{stock['Threshold']*100:.0f}%，考慮買進！")
                 stock['Notified'] = True
                 stock['Threshold'] -= 0.05
                 data_changed = True
